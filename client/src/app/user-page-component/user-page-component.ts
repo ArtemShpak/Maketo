805e2bd7b699c7caa394f1eb102c5404f531a037
Artem Shpak <artem6491@gmail.com>
2025-07-20 22:40:10 +0300

    Created routing

---
+++ client/src/app/user-page-component/user-page-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-user-page-component',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './user-page-component.html',
+  styleUrl: './user-page-component.css'
+})
+export class UserPageComponent {
+
+}
